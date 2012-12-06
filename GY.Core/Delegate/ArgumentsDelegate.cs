c6a213c1dd99949a0bf9d253b241550a447bed93
yangzhenyu <yangzhenyu2008@126.com>
2012-12-07 00:40:28 +0800

    添加数据对ibatis的操作的封装；及多数据库源支持的封装。

---
+++ GY.Core/Delegate/ArgumentsDelegate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GY.Core.Delegate
+{
+    /// <summary>
+    /// 委托类，把所有委托的对象放在些处
+    /// </summary>
+    public static class ArgumentsDelegate
+    {
+        /// <summary>
+        /// 无参无返回值委托.
+        /// </summary>
+        public delegate void NoneArgumentsDelegate();
+    }
+}
